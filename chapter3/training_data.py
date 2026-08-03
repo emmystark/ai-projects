3330902c4da024794e2c0b6d607f799301313816
emmystark <nkereuwememmanuel03@gmail.com>
2026-08-03 17:52:15 +0100

    chapter3_completed

---
+++ chapter3/training_data.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from nnfs.datasets import spiral_data
+
+import matplotlib.pyplot as plt
+
+x, y = spiral_data(samples=100, classes=3)
+
+# plt.scatter(x[:,0], x[:,1], c=y, cmap='brg')
+# plt.show()
+
+
+import nnfs 
+
+nnfs.init()
+
+n_inputs = 2
+n_neurons = 4
+
+weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+biases = np.zeros((1, n_neurons))
+
+print(weights)
+print(biases)
+
+# print(np.random.randn(2, 5))
+
+# print(np.zeros((2, 5)))
